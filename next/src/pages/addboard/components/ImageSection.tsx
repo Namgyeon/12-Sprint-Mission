0772aa2af4ea1361f1e4fe259c1360ac0e920506
Namgyeon <getam101@naver.com>
2025-01-26 01:29:31 +0900

    feat: image 삭제버튼 기능 추가

--- next/src/pages/addboard/components/ImageSection.tsx
+++ next/src/pages/addboard/components/ImageSection.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function ImageSection() {
   const [image, setImage] = useState<string | null>(null);
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]; // 첫번째 파일만 가져옴.(1개)
     if (file) {
       const reader = new FileReader(); // FileReader 브라우저에서 파일을 읽는데 사용되는 기본 API.
@@ -16,18 +16,33 @@ export default function ImageSection() {
     }
   };
 
+  const handleImageDelete = (e: React.MouseEvent<HTMLImageElement>) => {
+    setImage(null);
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>이미지</h2>
       <label htmlFor="image-upload" className={styles.label}>
         <div className={styles.labelContent}>
           {image ? (
-            <Image
-              src={image}
-              alt="상품이미지 미리보기"
-              width={282}
-              height={282}
-            />
+            <div className={styles.imageContainer}>
+              <Image
+                src={image}
+                alt="상품이미지 미리보기"
+                width={282}
+                height={282}
+              />
+              <Image
+                className={styles.delete}
+                src={"/images/ic_X.svg"}
+                alt="삭제 버튼이미지"
+                width={24}
+                height={24}
+                onClick={handleImageDelete}
+              />
+            </div>
           ) : (
             <>
               <Image
@@ -46,7 +61,7 @@ export default function ImageSection() {
           accept="image/png, image/jpeg"
           className={styles.input}
           placeholder="이미지 등록"
-          onChange={handleImageChange}
+          onChange={handleImageUpload}
         />
       </label>
     </div>
